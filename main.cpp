1aaa34c6deedcfd688cf27aab54778b762fd8b16
WzZ <3043187702@qq.com>
2025-09-06 23:27:08 +0800

    round1

--- main.cpp
+++ main.cpp
@@ -1,24 +1,45 @@
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
 #include "tgaimage.h"
 
-constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
-constexpr TGAColor green   = {  0, 255,   0, 255};
-constexpr TGAColor red     = {  0,   0, 255, 255};
-constexpr TGAColor blue    = {255, 128,  64, 255};
-constexpr TGAColor yellow  = {  0, 200, 255, 255};
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
+    bool steep = std::abs(ax - bx) < std::abs(ay - by);
+    if (steep) {
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
+    if (ax > bx) {
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+    for (int x=ax; x<=bx; x++) {
+        float t = (x-ax) / static_cast<float>(bx-ax);
+        int y = std::round(ay +t * (by - ay));
+        if (steep) 
+            framebuffer.set(y, x, color);
+        else 
+            framebuffer.set(x, y, color);
+    }
+}
 
 int main(int argc, char** argv) {
     constexpr int width  = 64;
     constexpr int height = 64;
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    int ax =  7, ay =  3;
-    int bx = 12, by = 37;
-    int cx = 62, cy = 53;
-
-    framebuffer.set(ax, ay, white);
-    framebuffer.set(bx, by, white);
-    framebuffer.set(cx, cy, white);
+    std::srand(std::time({}));
+    for (int i=0; i<(1<<24); i++) {
+        int ax = rand()%width, ay = rand()%height;
+        int bx = rand()%width, by = rand()%height;
+        TGAColor color = {
+            static_cast<std::uint8_t>(rand() % 255),
+            static_cast<std::uint8_t>(rand() % 255),
+            static_cast<std::uint8_t>(rand() % 255),
+            static_cast<std::uint8_t>(rand() % 255)
+        };
+        line(ax, ay, bx, by, framebuffer, color);
+    }
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
