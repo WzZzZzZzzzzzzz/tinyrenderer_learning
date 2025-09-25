71e9ea89be35d10223e14953c7724a7d5fd0ab19
WzZ <3043187702@qq.com>
2025-09-25 23:26:00 +0800

    african-head

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 #include "tgaimage.h"
 #include "model.h"
 
-constexpr int width  = 128;
-constexpr int height = 128;
+constexpr int width  = 1080;
+constexpr int height = 1080;
 
 constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
 constexpr TGAColor green   = {  0, 255,   0, 255};
@@ -43,32 +43,6 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
     }
 }
 
-// void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color) {
-//     if (ay > by) { std::swap(ax, bx); std::swap(ay, by);}
-//     if (ay > cy) { std::swap(ax, cx); std::swap(ay, cy);}
-//     if (by > cy) { std::swap(bx, cx); std::swap(by, cy);}
-//     int total_height = cy - ay;
-
-//     if (ay != by) {
-//         int segment_hight = by - ay;
-//         for (int y = ay; y <= by; y++) {
-//             int x1 = ax + ((cx - ax) * (y - ay)) / total_height;
-//             int x2 = ax + ((bx - ax) * (y - ay)) / segment_hight;
-//             for (int x = std::min(x1, x2); x < std::max(x1, x2); x++)
-//                 framebuffer.set(x, y, color);
-//         }
-//     }
-//     if (by != cy) {
-//         int segment_hight = cy - by;
-//         for (int y = by; y <= cy; y++) {
-//             int x1 = ax + ((cx - ax) * (y - ay)) / total_height;
-//             int x2 = bx + ((cx - bx) * (y - by)) / segment_hight;
-//             for (int x = std::min(x1, x2); x < std::max(x1, x2); x++)
-//                 framebuffer.set(x, y, color);
-//         }
-//     }
-// }
-
 double signed_triangle_area(int ax, int ay, int bx, int by, int cx, int cy) {
     return .5 * ((by - ay) * (bx + ax) + (cy - by) * (cx + bx) + (ay - cy) * (ax + cx));
 }
@@ -79,6 +53,7 @@ void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuf
     int bbmaxx = std::max(std::max(ax, bx), cx);
     int bbmaxy = std::max(std::max(ay, by), cy);
     double total_area = signed_triangle_area(ax, ay, bx, by, cx, cy);
+    if (total_area < 1) return;
 
 #pragma omp parallel for
     for (int x = bbminx; x <= bbmaxx; x++) {
@@ -90,6 +65,10 @@ void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuf
             framebuffer.set(x, y, color);
         }
     }
+}
+
+std::tuple<int, int> project(Vec3f v) {
+    return { (v.x + 1.) * width / 2, (v.y + 1.) * height / 2};
 }
 
 int main(int argc, char** argv) {
@@ -117,18 +96,16 @@ int main(int argc, char** argv) {
     const auto& faces = model.get_faces();
 
     for(const auto& face:faces) {
-        for(size_t i = 0; i < face.size(); i++) {
-            Vec3f v0 = vertices[face[i]];
-            Vec3f v1 = vertices[face[(i + 1) % face.size()]];
 
-            int x0 = (v0.x + 1.0)* width / 2.0;
-            int y0 = (v0.y + 1.0)* height / 2.0;
-            int x1 = (v1.x + 1.0)* width / 2.0;
-            int y1 = (v1.y + 1.0)* height / 2.0;
+        auto [ax, ay] = project(vertices[face[0]]);
+        auto [bx, by] = project(vertices[face[1]]);
+        auto [cx, cy] = project(vertices[face[2]]);
 
-            line(x0, y0, x1, y1, framebuffer, white);
-            framebuffer.set(x0, y0, red);
-        }
+        TGAColor rnd;
+        for (int c = 0; c < 3; c++) rnd[c] = std::rand()%255;
+            
+        triangle(ax, ay, bx, by, cx, cy, framebuffer, rnd);
+        
     }
     framebuffer.write_tga_file("framebuffer.tga");
 
