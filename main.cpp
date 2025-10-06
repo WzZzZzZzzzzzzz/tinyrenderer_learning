f4f203ec06b448b0ec49c5613f4d6a94dec7a769
WzZ <3043187702@qq.com>
2025-10-06 21:32:52 +0800

    barycentric coordinates homework

--- main.cpp
+++ main.cpp
@@ -7,13 +7,14 @@
 #include "tgaimage.h"
 #include "model.h"
 
-constexpr int width  = 1080;
-constexpr int height = 1080;
+constexpr int width  = 64;
+constexpr int height = 64;
 
 constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
+constexpr TGAColor black   = {  0,   0,   0, 255}; 
 constexpr TGAColor green   = {  0, 255,   0, 255};
 constexpr TGAColor red     = {  0,   0, 255, 255};
-constexpr TGAColor blue    = {255, 128,  64, 255};
+constexpr TGAColor blue    = {255,   0,   0, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
 void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
@@ -47,7 +48,7 @@ double signed_triangle_area(int ax, int ay, int bx, int by, int cx, int cy) {
     return .5 * ((by - ay) * (bx + ax) + (cy - by) * (cx + bx) + (ay - cy) * (ax + cx));
 }
 
-void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color) {
+void triangle(int ax, int ay, TGAColor az, int bx, int by, TGAColor bz, int cx, int cy, TGAColor cz, TGAImage &framebuffer) {
     int bbminx = std::min(std::min(ax, bx), cx);
     int bbminy = std::min(std::min(ay, by), cy);
     int bbmaxx = std::max(std::max(ax, bx), cx);
@@ -62,7 +63,11 @@ void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuf
             double beta = signed_triangle_area(x, y, cx, cy, ax, ay) / total_area;
             double gamma = signed_triangle_area(x, y, ax, ay, bx, by) / total_area;
             if (alpha < 0 || beta < 0 || gamma < 0) continue;
-            framebuffer.set(x, y, color);
+            TGAColor final_color;
+            final_color.bgra[0] = static_cast<std::uint8_t> (alpha * az.bgra[0] + beta * bz.bgra[0] + gamma * cz.bgra[0]);
+            final_color.bgra[1] = static_cast<std::uint8_t> (alpha * az.bgra[1] + beta * bz.bgra[1] + gamma * cz.bgra[1]);
+            final_color.bgra[2] = static_cast<std::uint8_t> (alpha * az.bgra[2] + beta * bz.bgra[2] + gamma * cz.bgra[2]);
+            framebuffer.set(x, y, final_color);
         }
     }
 }
@@ -75,10 +80,15 @@ int main(int argc, char** argv) {
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
     if (argc < 2) {
-        triangle(  7, 45, 35, 100, 45,  60, framebuffer, red);
-        triangle(120, 35, 90,   5, 45, 110, framebuffer, white);
-        triangle(115, 83, 80,  90, 85, 120, framebuffer, green);
+        int ax = 17, ay =  4;
+        int bx = 55, by = 39;
+        int cx = 23, cy = 59;
+        TGAColor az = red, bz = blue, cz = green;
 
+        triangle(ax, ay, az, bx, by, bz, cx, cy, cz, framebuffer);
+        triangle(ax * 2 / 3 + bx / 6 + cx / 6, ay * 2 / 3 + by / 6 + cy / 6, black,
+                 bx * 2 / 3 + ax / 6 + cx / 6, by * 2 / 3 + ay / 6 + cy / 6, black,
+                 cx * 2 / 3 + bx / 6 + ax / 6, cy * 2 / 3 + by / 6 + ay / 6, black, framebuffer);
         framebuffer.write_tga_file("framebuffer.tga");
         std::cout << "no add file" << std::endl;
         return 0;
@@ -104,7 +114,7 @@ int main(int argc, char** argv) {
         TGAColor rnd;
         for (int c = 0; c < 3; c++) rnd[c] = std::rand()%255;
             
-        triangle(ax, ay, bx, by, cx, cy, framebuffer, rnd);
+        // triangle(ax, ay, bx, by, cx, cy, framebuffer, rnd);
         
     }
     framebuffer.write_tga_file("framebuffer.tga");
