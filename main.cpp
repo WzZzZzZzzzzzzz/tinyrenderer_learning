39b5fb41e24994c5d14b9a1db9ca7b56f31c4966
WzZ <3043187702@qq.com>
2025-10-25 17:07:53 +0800

    shading

--- main.cpp
+++ main.cpp
@@ -24,10 +24,12 @@ extern std::vector<double> zbuffer;
 
 struct RandomShader : IShader {
     const Model &model;
-    TGAColor color = {};
+    vec3 l;
     vec3 tri[3];
 
-    RandomShader (const Model &m) : model(m) { }
+    RandomShader (const vec3 light, const Model &m) : model(m) {
+        l = normalized((ModelView * vec4{light.x, light.y, light.z, 0.}).xyz());
+    }
 
     virtual vec4 vertex(const int face, const int vert) {
         vec3 v = model.vert(face,vert);
@@ -37,7 +39,15 @@ struct RandomShader : IShader {
     }
 
     virtual std::pair<bool, TGAColor> fragment(const vec3 bar) const {
-        return {false, color};
+        TGAColor gl_FragColor = {255, 255, 255, 255};
+        vec3 n = normalized(cross(tri[1] - tri[0], tri[2] - tri[0]));
+        vec3 r = normalized(n * (n * l) * 2 - l);
+        double ambient = .3;
+        double diff = std::max(0., n * l);
+        double spec = std::pow(std::max(r.z, 0.), 35);
+        for (int channel : {0, 1, 2})
+            gl_FragColor[channel] *= std::min(1., ambient + .4 * diff + .9 * spec);
+        return {false, gl_FragColor};
     }
 };
 
@@ -59,6 +69,7 @@ int main(int argc, char** argv) {
     }
     constexpr int width  = 1024;
     constexpr int height = 1024;
+    constexpr vec3 light{1, 1, 1};
     constexpr vec3 eye{-1, 0, 2};
     constexpr vec3 center{0, 0, 0};
     constexpr vec3 up{0, 1, 0};
@@ -71,11 +82,8 @@ int main(int argc, char** argv) {
 
     for (int m = 1; m < argc; m++) {
         Model model(argv[m]);
-        RandomShader shader(model);
+        RandomShader shader(light, model);
         for (int f = 0; f < model.nfaces(); f++) {
-            shader.color = {static_cast<unsigned char>(std::rand()%255),
-                            static_cast<unsigned char>(std::rand()%255),
-                            static_cast<unsigned char>(std::rand()%255), 255};
             Triangle clip = {shader.vertex(f, 0),
                             shader.vertex(f, 1),
                             shader.vertex(f, 2)};
